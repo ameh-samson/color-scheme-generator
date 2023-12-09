d287b31440cacb2dcae60a605706810f1032a37c
Samson Ameh <amehsam69@gmail.com>
2023-12-09 20:50:13 +0100

    render hexCode function was added

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
-const form = document.querySelector(".form");
+// gets the required element from the HTML
+
 const colorPicker = document.querySelector("#color-picker");
 const selectMode = document.querySelector("#select-mode");
-const generateBtn = document.querySelector("#generateBtn");
+const form = document.querySelector(".form");
 const colorsDivContainer = document.querySelector(".colors-div-container");
+const hexCodesContainer = document.querySelector(".hex-codes-container");
 
+// listen to a submit event from the form
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   generateColorScheme();
 });
 
+// generates the color scheme
 function generateColorScheme() {
   const colorSelected = colorPicker.value;
   const modeSelected = selectMode.value;
   const encodedColor = encodeURIComponent(colorSelected);
   const encodedMode = encodeURIComponent(modeSelected);
 
+  // fetches the colorScheme from the color API with GET method
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${encodedColor}&mode=${encodedMode}&count=5`,
     { method: "GET" }
   )
     .then((res) => res.json())
     .then((data) => {
-      displayColorScheme(data);
+      renderColorScheme(data); // calls the function that renders the color to the DOM with the the data return passed into it
+      renderHexCode(data); // calls the function that renders the hexCode of the color to the DOM
     });
 }
 
-function displayColorScheme(data) {
+function renderColorScheme(data) {
   colorsDivContainer.innerHTML = "";
   data.colors.forEach((color) => {
     colorsDivContainer.innerHTML += `
       <div class="colors-div" style="background-color: ${color.hex.value};"></div>
       `;
   });
-
-  const colorsDiv = document.querySelectorAll(".colors-div");
-  colorsDiv.forEach((colorDiv) => {
-    colorDiv.addEventListener("click", () => {
-      const hexCode = colorsDiv.style.backgroundColor;
-      copyHexCodeToClipboard(hexCode);
-      alert(`Hex code "${hexCode}" copied to clipboard!`);
-    });
-  });
 }
 
-function copyHexCodeToClipboard(hexCode) {
-  const textarea = document.createElement("textarea");
-  textarea.value = hexCode;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand("copy");
-  document.body.removeChild(textarea);
+// renders the hex code to the DOM
+function renderHexCode(data) {
+  hexCodesContainer.innerHTML = "";
+  data.colors.forEach((color) => {
+    hexCodesContainer.innerHTML += `
+      <P>${color.hex.value}</P>
+    `;
+  });
 }
